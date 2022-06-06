e81ba1984a32407292964fc39550dbb171e0fada
Fazlur Rahman Khan <fyndfrk@gmail.com>
2022-06-06 08:41:41 +0530

    Update ubu-pre.sh

--- ubu-pre.sh
+++ ubu-pre.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+apt-get install -y curl openssh-server vim 
+sed -e 's/^.*PermitRootLogin prohibit-password/PermitRootLogin yes/g' -i  /etc/ssh/sshd_config
+systemctl restart sshd 
+systemctl disable --now ufw
+
 swapoff -a
 sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 modprobe br_netfilter
