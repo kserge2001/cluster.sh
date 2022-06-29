94d55dc83eef38a0125611a56d15c3dea6cca1c7
Fazlur Rahman Khan <fyndfrk@gmail.com>
2022-06-29 14:25:37 +0530

    Update ubu-pre.sh

--- ubu-pre.sh
+++ ubu-pre.sh
@@ -53,6 +53,6 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
-sudo apt-get install -y kubelet=1.23.0-00  kubeadm=1.23.0-00  kubectl=1.23.0-00 
+sudo apt-get install -y kubelet=1.23.5-00  kubeadm=1.23.5-00  kubectl=1.23.5-00 
 
 
