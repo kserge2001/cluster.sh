79c241bab7e6ba75ada2ca17b480ce31c09f5b3a
Fazlur Rahman Khan <fyndfrk@gmail.com>
2022-09-29 22:07:22 +0530

    Update upgrade-ts.sh

--- upgrade-ts.sh
+++ upgrade-ts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo kubectl apply -f "https://raw.githubusercontent.com/fazlurkh/pods/main/ts.yaml" &> /dev/null
+sudo kubectl apply -f "https://raw.githubusercontent.com/fazlurkh/pods/main/ts.yaml" >> /dev/null
 sudo kubectl taint node `kubectl get nodes --no-headers | tail -2 | awk '{print $1}'` cka=test:NoSchedule --overwrite
-&> /dev/null
-sudo kubectl run ckaexam --image nginx  &> /dev/null
+>> /dev/null
+sudo kubectl run ckaexam --image nginx  >> /dev/null
 sudo mkdir -p ~/backup
-sudo cp -p ~/.kube/config ~/backup/  &> /dev/null
+sudo cp -p ~/.kube/config ~/backup/  >> /dev/null
 sudo sed -e 's/6443/9663/g' -i ~/.kube/config
-sudo cp -p /etc/kubernetes/manifests/kube-apiserver.yaml ~/backup/ &> /dev/null
+sudo cp -p /etc/kubernetes/manifests/kube-apiserver.yaml ~/backup/ >> /dev/null
 sudo sed -e 's/RBAC/RBAC,FAZLUR/g' -i /etc/kubernetes/manifests/kube-apiserver.yaml
 sudo systemctl disable --now kubelet &> /dev/null
 sudo echo "Executing steps to upgrade your cluster... Please standby... "
