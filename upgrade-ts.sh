9e301d9b05d746de59dc4c0e138528d7756fac10
Fazlur Rahman Khan <fyndfrk@gmail.com>
2022-09-29 21:41:30 +0530

    Update and rename ts.sh to upgrade-ts.sh

---
+++ upgrade-ts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo kubectl apply -f "https://raw.githubusercontent.com/fazlurkh/pods/main/ts.yaml" >> /dev/null
+sudo kubectl taint node `kubectl get nodes --no-headers | tail -2 | awk '{print $1}'` cka=test:NoSchedule >> /dev/null
+sudo kubectl run ckaexam --image nginx >> /dev/null
+sudo mkdir -p ~/backup
+sudo cp -i ~/.kube/config ~/backup/ >> /dev/null
+sudo sed -e 's/6443/9663/g' -i ~/.kube/config
+sudo cp -i /etc/kubernetes/manifests/kube-apiserver.yaml ~/backup/ >> /dev/null
+sudo sed -e 's/RBAC/RBAC,FAZLUR/g' -i /etc/kubernetes/manifests/kube-apiserver.yaml
+sudo systemctl disable --now kubelet >> /dev/null
+sudo echo " Executing steps to upgrade your cluster... Please standby... "
+sudo sleep 10
+sudo echo " Opps ... Looks like your cluster is broken, Please fix it ... "
