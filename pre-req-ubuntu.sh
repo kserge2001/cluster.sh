60302ad9a5ff152f7e4328450818e8fd3c43c918
kserge2001 <kserge2001@yahoo.fr>
2022-10-20 14:43:25 -0500

    Update pre-req-ubuntu.sh

--- pre-req-ubuntu.sh
+++ pre-req-ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+##Nodes set up
+
 apt-get install -y curl openssh-server vim 
 sed -e 's/^.*PermitRootLogin prohibit-password/PermitRootLogin yes/g' -i  /etc/ssh/sshd_config
 systemctl restart sshd 
